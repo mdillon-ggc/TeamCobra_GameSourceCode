50da88196a23cad10531423fb39405a8eaae6deb
hluna2 <hluna2@ggc.edu>
2025-11-14 21:48:04 -0500

    Update Useable.java
    Add use() method.

--- Useable.java
+++ Useable.java
@@ -1,17 +1,27 @@
 public class Useable extends Item{
     private int maxUseAmount;
     //constructor
-    public Useable(String itemID, String itemName, String itemDesc, String itemType, int itemValue, int maxStack) {
-        super(itemID, itemName, itemDesc, itemType, itemValue, maxStack);
+    public Useable(String itemID, String itemName, String itemDesc, int itemValue, int maxStack) {
+        super(itemID, itemName, itemDesc, itemValue, maxStack);
         this.maxUseAmount = itemValue;
-    }
-
-    public int getMaxUseAmount() {
-        return maxUseAmount;
     }
 
     @Override
     public boolean isUseable(){
         return true;
+    }
+    
+    public int getMaxUseAmount()
+    {
+    	return maxUseAmount;
+    }
+    
+    public void use() {
+        if (currentStack > 0) {
+            currentStack--;
+            System.out.println("Used " + itemName + ". Remaining: " + currentStack);
+        } else {
+            System.out.println(itemName + " cannot be used, none left!");
+        }
     }
 }
