d0b6e49ceb211d2f7557c527c264b78af47135cc
hluna2 <hluna2@ggc.edu>
2025-11-14 21:55:08 -0500

    Update Item.java
    Implement cloneable and add toString() and use() method.

--- Item.java
+++ Item.java
@@ -1,22 +1,33 @@
-public abstract class Item {
-    private String itemID;
-    private String itemName;
-    private String itemDesc;
-    private String itemType;
-    private int itemValue;
-    private int maxStack;
+public abstract class Item implements Cloneable {
+    protected String itemID;
+    protected String itemName;
+    protected String itemDesc;
+    protected String itemType;
+    protected int itemValue;
+    protected int maxStack;
+    protected int currentStack = 1; //default 1
 
 
     //constructor
-    public Item(String itemID, String itemName, String itemDesc, String itemType, int itemValue, int maxStack) {
+    public Item(String itemID, String itemName, String itemDesc, int itemValue, int maxStack) {
         this.itemID = itemID;
         this.itemName = itemName;
         this.itemDesc = itemDesc;
-        this.itemType = itemType;
         this.itemValue = itemValue;
         this.maxStack = maxStack;
 
     }
+    
+    @Override
+    public Item clone() {
+        try {
+            return (Item) super.clone(); // shallow copy is fine for primitives/Strings
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }    
+    
     //getters
     public String getItemID(){
         return itemID;
@@ -30,16 +41,28 @@ public abstract class Item {
         return itemDesc;
     }
 
-    public String getItemType() {
-        return itemType;
-    }
-
     public int getItemValue() {
         return itemValue;
     }
 
+    public String getItemType() {
+        return itemType;
+    }
+    
     public int getMaxStack(){
         return maxStack;
+    }
+    
+    public int getCurrentStack(){
+        return currentStack;
+    }
+    
+    public void setMaxStack(int maxStack) {
+    	this.maxStack = maxStack;
+    }
+    
+    public void setCurrentStack(int currentStack) {
+    	this.currentStack = currentStack;
     }
 
     public boolean isEquipable(){
@@ -50,6 +73,15 @@ public abstract class Item {
     }
     public boolean isUseable(){
         return false;
+    }
+
+    public void use() {
+        System.out.println(itemName + "cannot be used.");
+    }
+    
+    @Override
+    public String toString() {
+        return this.getItemName(); // or any property you want to display
     }
 
 
