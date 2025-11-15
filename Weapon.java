38d62bf6f4902242f5428d28ef484fc491cf5f31
hluna2 <hluna2@ggc.edu>
2025-11-14 21:48:39 -0500

    Update Weapon.java
    Fix error in constructor.

--- Weapon.java
+++ Weapon.java
@@ -2,15 +2,14 @@ public class Weapon extends Item {
     private int dealDamage;
 
     //constructor
-    public Weapon(String itemID, String itemName, String itemDesc, String itemType, int itemValue, int maxStack) {
-        super(itemID, itemName, itemDesc, itemType, itemValue, maxStack);
+    public Weapon(String itemID, String itemName, String itemDesc, int itemValue) {
+        super(itemID, itemName, itemDesc, itemValue, 1); //weapons not stackable
         this.dealDamage = itemValue;
     }
     //getter
     public int getDealDamage(){
         return dealDamage;
     }
-
     @Override
     public boolean isEquipable(){
         return true;
